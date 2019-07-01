b6bb6e49487823ce6b04a79cb87278f07fb1df50
LAPTOP-C8F0QODP\Joël <Jsmjohn@hotmail.com>
2019-07-01 16:57:33 +0200

    NavMesh

---
+++ ON_LINE MULTI_PLAYER/Assets/Farmer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Farmer : MonoBehaviour
+{
+    public GameObject house;
+    public Transform goal;
+    public NavMeshAgent agent;
+    public float goalReached;
+    public float maxcooldown;
+    public float cooldown;
+    public bool go;
+    public bool auto;
+
+    public void Awake()
+    {
+        house = GameObject.FindGameObjectWithTag("House");
+        cooldown = maxcooldown;
+        if(house != null)
+        {
+            goal = house.transform;
+        }
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        float dis = Vector3.Distance(transform.position, goal.position);
+        if (go)
+        {
+            agent.SetDestination(goal.position);
+        }
+        else
+        {
+            agent.SetDestination(house.transform.position);
+        }
+        if(dis <= goalReached)
+        {
+            cooldown -= Time.deltaTime;
+            if (cooldown <= 0)
+            {
+                goal = goal.GetComponent<Goal>().nextGoal;
+                if (auto == false)
+                {
+                    go = false;
+                }
+                cooldown = maxcooldown;
+            }
+        }
+    }
+}
---
+++ ON_LINE MULTI_PLAYER/Assets/SpawnFarmer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFarmer : MonoBehaviour
+{
+    public GameObject farmer;
+    public GameObject houseFarmer;
+    public bool spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawn)
+        {
+            Instantiate(farmer, houseFarmer.transform.position, Quaternion.identity);
+            spawn = false;
+        }
+    }
+    public void SetFarmer()
+    {
+        if(farmer.GetComponent<Farmer>().house != null)
+        {
+            farmer.GetComponent<Farmer>().house = houseFarmer;
+        }
+    }
+}
